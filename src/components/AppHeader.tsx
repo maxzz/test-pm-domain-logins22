9e5232e084034e2250603e6f7f76b0cfed7513dd
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-07 16:01:47 -0700

    WIP. AppHeader. add curves.ai

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,6 +1,6 @@
 import { IconHero, IconHIDLogo } from './UI/UIIcons';
 import { a, easings, useSpring } from '@react-spring/web';
-import curvesSvg from '../assets/curves.svg';
+import curvesSvg from '../assets/sources/curves.svg';
 
 export const textShadow = { textShadow: '1px 1px 2px #000' };
 export const elevation4Shadow = { boxShadow: '0 2px 4px -1px rgb(0 0 0 / 20%), 0 4px 5px 0 rgb(0 0 0 / 14%), 0 1px 10px 0 rgb(0 0 0 / 12%)' };
