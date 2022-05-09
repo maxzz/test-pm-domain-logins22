bd6da1bfb269c9a212e932326c6ccd5ca9c19088
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-08 19:11:16 -0700

    WIP. Section1_LoginArea. add ScreenSearch component

--- src/components/sections/Section1_LoginArea.tsx
+++ src/components/sections/Section1_LoginArea.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import { useAtom, useAtomValue } from 'jotai';
 import { showSearchPageAtom } from '@/store/store';
 import { a, easings, useSpring } from '@react-spring/web';
@@ -5,31 +6,54 @@ import { classNames } from '@/utils/classnames';
 
 const boxShadow = { boxShadow: '0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12)', };
 
-function PreviewContainer() {
-    const styles = useSpring({ scale: 1, from: { scale: 2 }, config: { duration: 2000, easing: easings.easeInOutElastic } });
+// function PreviewContainer() {
+//     const styles = useSpring({ scale: 1, from: { scale: 2 }, config: { duration: 2000, easing: easings.easeInOutElastic } });
+//     return (
+//         <div className="bg-slate-400 overflow-hidden      text-center p-12" style={{ ...boxShadow, transition: "all .2s" }}>
+//             <a.div style={styles} className="h-full object-cover">
+//                 Place more here
+//             </a.div>
+//         </div>
+//     );
+// }
+
+const iconShadow = { filter: 'drop-shadow(1px 1px 1px #0002)', };
+
+function LoginTitle({ label = '', logo, className, ...rest }: { label?: string; logo: ReactNode; } & React.HTMLAttributes<HTMLInputElement>) {
+    const styles = useSpring({
+        from: {
+            scaleX: 0,
+            opacity: 0,
+        }, to: {
+            scaleX: 1,
+            opacity: 1,
+        },
+    });
     return (
-        <div className="bg-slate-400 overflow-hidden      text-center p-12" style={{ ...boxShadow, transition: "all .2s" }}>
-            <a.div style={styles} className="h-full object-cover">
-                Place more here
+        <div className={classNames("px-4 py-4 flex items-center justify-between border-b border-slate-400 shadow select-none", className)}>
+            <div className="font-bold">{label}</div>
+            <a.div
+                style={styles}
+                className="px-4 pb-2 w-16 h-16 text-5xl flex items-center justify-center text-slate-50 bg-slate-300 border-slate-50 border-4 rounded-md"
+            >
+                {logo}
             </a.div>
         </div>
     );
 }
 
-const iconShadow = { filter: 'drop-shadow(1px 1px 1px #0002)', };
-
-function FieldUser({ suffix = '' }: { suffix?: string; }) {
+function FieldUser({ fieldId = '', placeholder = ' ' }: { fieldId?: string; placeholder?: string; }) {
     return (
         <label className="relative">
             <input
                 className="py-1.5 w-full peer float-input"
-                id={`user${suffix}`}
+                id={`user${fieldId}`}
                 type="text"
-                placeholder="Username"
+                placeholder={placeholder}
             />
 
             <div className="float-label">
-                Username
+                {placeholder}
             </div>
         </label>
     );
@@ -53,38 +77,36 @@ function FieldPass({ suffix = '' }: { suffix?: string; }) {
     );
 }
 
-function LoginTitle({ label = '', className, ...rest }: { label?: string; } & React.HTMLAttributes<HTMLInputElement>) {
-    const styles = useSpring({
-        from: {
-            scaleX: 0,
-            opacity: 0,
-        }, to: {
-            scaleX: 1,
-            opacity: 1,
-        },
-    });
-    return (
-        <div className={classNames("px-4 py-4 flex items-center justify-between border-b border-slate-400 shadow select-none", className)}>
-            <div className="font-bold">Login A</div>
-            <a.div style={styles} className="px-4 w-16 h-16 text-5xl flex items-center justify-center text-slate-50 bg-slate-300 border-slate-50 border-4 rounded-md">A</a.div>
-        </div>
-    );
-}
-
 function FieldSubmit({ label = '', className, ...rest }: { label?: string; } & React.HTMLAttributes<HTMLInputElement>) {
     return (
         <input className={classNames('px-4 py-2 hover:bg-red-500 active:scale-[.97] border-slate-600 border rounded', className)} type="button" value={label} {...rest} />
     );
 }
 
-function LoginView({ suffix = '' }: { suffix?: string; }) {
+function ScreenLogin({ suffix = '' }: { suffix?: string; }) {
     return (
-        <form className="pb-4 flex flex-col space-y-4 rounded-sm bg-slate-200" style={boxShadow}>
-            <LoginTitle />
+        <form id="tm-login-a-form" className="pb-4 flex flex-col space-y-4 rounded-sm bg-slate-200" style={boxShadow}>
+            <LoginTitle label="Login A" logo={'A'} />
 
             <div className="px-4 pt-4 pb-2 w-72 flex flex-col space-y-8">
-                <FieldUser suffix={suffix} />
+                <FieldUser fieldId={`user${suffix}`} placeholder="Username" />
                 <FieldPass suffix={suffix} />
+            </div>
+
+            <div className="px-4 self-end">
+                <FieldSubmit className="" label="Login" />
+            </div>
+        </form>
+    );
+}
+
+function ScreenSearch({ suffix = '' }: { suffix?: string; }) {
+    return (
+        <form id="tm-search-form" className="pb-4 flex flex-col space-y-4 rounded-sm bg-slate-200" style={boxShadow}>
+            <LoginTitle label="Search" logo={<div className="text-orange-500">S</div>} />
+
+            <div className="px-4 pt-4 pb-2 w-72 flex flex-col space-y-8">
+                <FieldUser fieldId={`seacrh${suffix}`} placeholder="Search" />
             </div>
 
             <div className="px-4 self-end">
@@ -98,12 +120,11 @@ function TempControls() {
     const [showSearch, setShowSearch] = useAtom(showSearchPageAtom);
     return (
         <div className="p-4">
-            <label className="flex items-center space-x-2">
+            <label className="flex items-center space-x-2 select-none">
                 <input className="form-checkbox rounded" type="checkbox" checked={showSearch} onChange={() => setShowSearch((v) => !v)} />
                 <div className="">Search page</div>
             </label>
         </div>
-
     );
 }
 
@@ -116,7 +137,7 @@ export function Section1_LoginArea() {
                 {/* <div className="mt-4 grid grid-cols-2 gap-4"> */}
                 {/* <PreviewContainer /> */}
 
-                {showSearch ? null : <LoginView suffix={'-2'} />}
+                {showSearch ? <ScreenSearch /> : <ScreenLogin suffix={'-2'} />}
             </div>
 
             <TempControls />
