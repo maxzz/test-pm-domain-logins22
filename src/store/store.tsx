5793c30e555c066154cefedaa961850a22af702d
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-10 16:54:21 -0700

    WIP. store. add credential atoms

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -9,10 +9,20 @@ namespace Storage {
 
     type Store = {
         showSearchPage: boolean;
+        loginAuser: string;
+        loginApass: string;
+        loginBuser: string;
+        loginBpass: string;
     };
+
+
 
     export let initialData: Store = {
         showSearchPage: false,
+        loginAuser: '',
+        loginApass: '',
+        loginBuser: '',
+        loginBpass: '',
     };
 
     function load() {
@@ -30,6 +40,10 @@ namespace Storage {
     export const save = debounce(function _save(get: Getter) {
         let newStore: Store = {
             showSearchPage: get(showSearchPageAtom),
+            loginAuser: get(loginAuserAtom),
+            loginApass: get(loginApassAtom),
+            loginBuser: get(loginBuserAtom),
+            loginBpass: get(loginBpassAtom),
         };
         localStorage.setItem(KEY, JSON.stringify(newStore));
     }, 1000);
@@ -38,3 +52,8 @@ namespace Storage {
 //#endregion LocalStorage
 
 export const showSearchPageAtom = atomWithCallback(Storage.initialData.showSearchPage, ({get}) => Storage.save(get));
+
+export const loginAuserAtom = atomWithCallback(Storage.initialData.loginAuser, ({get}) => Storage.save(get));
+export const loginApassAtom = atomWithCallback(Storage.initialData.loginApass, ({get}) => Storage.save(get));
+export const loginBuserAtom = atomWithCallback(Storage.initialData.loginBuser, ({get}) => Storage.save(get));
+export const loginBpassAtom = atomWithCallback(Storage.initialData.loginBpass, ({get}) => Storage.save(get));
