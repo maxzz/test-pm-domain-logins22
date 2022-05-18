9c207c371c36f2309d40d48e9e48fb14b81fe05a
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-18 02:56:10 -0700

    WIP. footer. tweak NavLinks trail animation

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -123,11 +123,17 @@ function NavLinks() {
 const Trail: React.FC<{ open: boolean; }> = ({ open, children }) => {
     const items = React.Children.toArray(children);
     const trail = useTrail(items.length, {
+        from: {
+            opacity: 0,
+            x: 20,
+            height: 0
+        },
+        to: {
+            opacity: open ? 1 : 0,
+            x: open ? 0 : 20,
+            height: open ? 70 : 0,
+        },
         config: { mass: 5, tension: 2000, friction: 200 },
-        opacity: open ? 1 : 0,
-        x: open ? 0 : 20,
-        height: open ? 110 : 0,
-        from: { opacity: 0, x: 20, height: 0 },
     });
     return (
         <div>
