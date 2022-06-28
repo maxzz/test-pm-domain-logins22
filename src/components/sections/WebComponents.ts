8e031bb4898e4488c1c2d9461aa1053c383d14df
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-27 18:53:08 -0700

    WIP. web-components. add tailwind styles to custom element

--- src/components/sections/WebComponents.ts
+++ src/components/sections/WebComponents.ts
@@ -1,12 +1,25 @@
+import styles from "../../index.css?inline";
+
+//console.log('styles', styles);
+
 class Example extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({ mode: "open" });
 
+        const styleNode = document.createElement('style');
+        styleNode.innerHTML = styles;
+        shadow.appendChild(styleNode);
+
         const button = document.createElement("button");
         button.onclick = () => alert("Hi");
         button.innerText = "Hello";
-        button.className = "bg-primary-100";
+
+        const className = "bg-primary-500";
+        button.className = className;
+        //button.className = "bg-primary-100";
+        //const classNames = '<div class="bg-primary-500"></div>';
+
         shadow.append(button);
     }
 }
