af4b885fd53a388f7622975e42f2489af4eb65d8
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-30 16:50:29 -0700

    WIP. web-components. remove unused react-to-webcomponent prop-types

--- src/components/sections/A1_Forms.tsx
+++ src/components/sections/A1_Forms.tsx
@@ -82,7 +82,7 @@ function FieldSubmit({ label = '', className, ...rest }: { label?: string; } & R
     return (
         <button
             className={classNames(
-                'px-4 py-1.5 hover:bg-slate-300 focus:bg-slate-300 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-slate-500 active:scale-[.97] border-slate-600 border rounded select-none',
+                'px-4 py-1.5 hover:bg-slate-300 focus:bg-slate-300 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-slate-500 active:scale-[.97] border-slate-400 border rounded shadow-sm select-none',
                 className
             )}
             {...rest}
