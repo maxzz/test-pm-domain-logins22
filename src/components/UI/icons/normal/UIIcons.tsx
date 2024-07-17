b4407f75785442256ef366dc8512a366235ba0be
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-16 19:19:50 -0700

    WIP. update. normal icons. fix tw cusomization

--- src/components/UI/icons/normal/UIIcons.tsx
+++ src/components/UI/icons/normal/UIIcons.tsx
@@ -2,9 +2,9 @@ import { forwardRef, HTMLAttributes, SVGProps } from "react";
 import { classNames } from "@/utils/classnames";
 
 export function IconHIDLogo(props: SVGProps<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
-    const { title, ...rest } = props;
+    const { title, className, ...rest } = props;
     return (
-        <svg fill="currentColor" viewBox="0 0 125.2 33.4" {...rest}>
+        <svg className={classNames("fill-current", className)} viewBox="0 0 125.2 33.4" {...rest}>
             {title && <title>{title}</title>}
             <path d="M79.96 33.16V.25c10.24 0 20.24-.68 30.09.2a16.09 16.09 0 0 1 15 16.47c0 8.41-5.76 15.23-15.27 16.05-9.75.84-19.58.19-29.82.19ZM96.22 3.21v27.07c6.61 1.08 9.56-.46 10.37-6.29a51.35 51.35 0 0 0 .06-13.9c-.87-6.28-3.5-7.74-10.43-6.88ZM0 32.8V.29h15.89v13.92h12V.34h16.05V32.7H28.03V19.31H16.11V32.8ZM54.24 32.86V.39h15.65v32.47Z" />
         </svg>
@@ -44,18 +44,18 @@ export function IconSearch({ title, ...rest }: SVGProps<SVGSVGElement> & HTMLAtt
     );
 }
 
-export function IconCPass({ title, ...rest }: SVGProps<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
+export function IconCPass({ title, className, ...rest }: SVGProps<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
     return (
-        <svg viewBox="0 0 24 24" fill="currentColor" {...rest}>
+        <svg className={classNames("fill-current", className)} viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
             <path d="M4.993 12.984a1 1 0 0 0-.531 1.848L7.15 17.52a1 1 0 1 0 1.414-1.415l-1.121-1.12h7.55a1 1 0 0 0 0-2h-10Zm14.014-1.968a1 1 0 0 0 .531-1.848L16.85 6.48a1 1 0 0 0-1.414 1.415l1.121 1.12h-7.55a1 1 0 0 0 0 2h10Z" />
         </svg>
     );
 }
 
-export function IconLogin({ title, ...rest }: SVGProps<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
+export function IconLogin({ title, className, ...rest }: SVGProps<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
     return (
-        <svg viewBox="0 0 32 32" fill="currentColor" {...rest}>
+        <svg className={classNames("fill-current", className)} viewBox="0 0 32 32" {...rest}>
             {title && <title>{title}</title>}
             <path d="M7 30a5 5 0 1 1 5-5a5.006 5.006 0 0 1-5 5zm0-8a3 3 0 1 0 3 3a3.003 3.003 0 0 0-3-3z" />
             <path d="M23 30a5 5 0 1 1 5-5a5.006 5.006 0 0 1-5 5zm0-8a3 3 0 1 0 3 3a3.003 3.003 0 0 0-3-3z" />
@@ -97,9 +97,9 @@ export function IconGithubLogo(props: SVGProps<SVGSVGElement> & HTMLAttributes<S
 // })
 
 // export function IconImagePlus(props: SVGProps<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
-//     const { title, ...rest } = props;
+//     const { title, className, ...rest } = props;
 //     return (
-//         <svg fill="currentColor" viewBox="0 0 24 24" {...rest}>
+//         <svg className={classNames("fill-current", className)} viewBox="0 0 24 24" {...rest}>
 //             {title && <title>{title}</title>}
 //             <path d="M19 10a1 1 0 0 0-1 1v3.38l-1.48-1.48a2.79 2.79 0 0 0-3.93 0l-.7.71l-2.48-2.49a2.79 2.79 0 0 0-3.93 0L4 12.61V7a1 1 0 0 1 1-1h8a1 1 0 0 0 0-2H5a3 3 0 0 0-3 3v12.22A2.79 2.79 0 0 0 4.78 22h12.44a2.88 2.88 0 0 0 .8-.12a2.74 2.74 0 0 0 2-2.65V11A1 1 0 0 0 19 10zM5 20a1 1 0 0 1-1-1v-3.57l2.89-2.89a.78.78 0 0 1 1.1 0L15.46 20zm13-1a1 1 0 0 1-.18.54L13.3 15l.71-.7a.77.77 0 0 1 1.1 0L18 17.21zm3-15h-1V3a1 1 0 0 0-2 0v1h-1a1 1 0 0 0 0 2h1v1a1 1 0 0 0 2 0V6h1a1 1 0 0 0 0-2z" />
 //         </svg>
@@ -107,21 +107,21 @@ export function IconGithubLogo(props: SVGProps<SVGSVGElement> & HTMLAttributes<S
 // }
 
 export function IconDownload(props: SVGProps<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
-    const { title, ...rest } = props;
+    const { title, className, ...rest } = props;
     return (
-        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} {...rest}>
+        <svg className={classNames("fill-none text-current stroke-2", className)} strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+            <path d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
         </svg>
     );
 }
 
 export function IconClipboard(props: SVGProps<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
-    const { title, ...rest } = props;
+    const { title, className, ...rest } = props;
     return (
-        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} {...rest}>
+        <svg className={classNames("fill-none text-current stroke-2", className)} strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24" {...rest}>
             {title && <title>{title}</title>}
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+            <path d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
         </svg>
     );
 }
