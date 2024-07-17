01381c43f44b41d78652fba9e62d327a385f111b
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-16 19:27:32 -0700

    WIP. update. normal icons. add svgRoundedCaps

--- src/components/UI/icons/normal/90-icon-props.tsx
+++ src/components/UI/icons/normal/90-icon-props.tsx
@@ -1,3 +1,8 @@
-import { SVGProps, HTMLAttributes } from "react";
+import { SVGProps, HTMLAttributes, SVGAttributes } from "react";
 
 export type IconProps = SVGProps<SVGSVGElement> & HTMLAttributes<SVGSVGElement>;
+
+export const svgRoundedCaps: SVGAttributes<SVGElement> = {
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+};
