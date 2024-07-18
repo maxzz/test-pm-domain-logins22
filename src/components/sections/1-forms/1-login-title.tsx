9e859d086e5229780b0a543426b9df96bb74b154
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-17 18:38:54 -0700

    WIP. update. forms. update LoginTitle

--- src/components/sections/1-forms/1-login-title.tsx
+++ src/components/sections/1-forms/1-login-title.tsx
@@ -1,6 +1,11 @@
-import { ReactNode } from "react";
+import { HTMLAttributes, ReactNode } from "react";
 import { a, useSpring } from "@react-spring/web";
 import { classNames } from "@/utils";
+
+type LoginTitleProps = HTMLAttributes<HTMLDivElement> & {
+    label: ReactNode;
+    logo: ReactNode;
+};
 
 const font = {
     fontFamily: 'Source Sans Pro, sans-serif',
@@ -12,17 +17,34 @@ const font = {
     //transform: 'scaleY(1.2)',
 };
 
-export function LoginTitle({ label, logo, className, ...rest }: { label: React.ReactNode; logo: ReactNode; } & React.HTMLAttributes<HTMLDivElement>) {
-    const styles = useSpring({ from: { scale: 0, borderWidth: '4px', opacity: 0 }, to: { scale: 1, borderWidth: '1px', opacity: 1, }, delay: 400, });
+const titleClasses = "\
+px-4 py-4 \
+bg-slate-200 border-slate-200 \
+border-b \
+rounded-t-sm shadow \
+select-none \
+flex items-center justify-between";
+
+const logoClasses = "\
+px-4 size-16 text-5xl \
+text-slate-50 bg-slate-300/40 border-slate-50 \
+border-4 rounded-md \
+flex items-center justify-center";
+
+export function LoginTitle({ label, logo, className, ...rest }: LoginTitleProps) {
+    const styles = useSpring({
+        from: { scale: 0, borderWidth: '4px', opacity: 0 },
+        to: { scale: 1, borderWidth: '1px', opacity: 1, },
+        delay: 400,
+    });
+
     return (
-        <div className={classNames("px-4 py-4 flex items-center justify-between border-b border-slate-200 bg-slate-200 rounded-t-sm shadow select-none", className)} {...rest}>
+        <div className={classNames(titleClasses, className)} {...rest}>
             <div className="font-bold" style={font}>
                 {label}
             </div>
 
-            <a.div style={styles}
-                className="px-4 w-16 h-16 text-5xl flex items-center justify-center text-slate-50 bg-slate-300/40 border-slate-50 border-4 rounded-md"
-            >
+            <a.div style={styles} className={logoClasses}>
                 {logo}
             </a.div>
         </div>
