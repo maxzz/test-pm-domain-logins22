70585271ca4a649640e5199985415c0a396b14db
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-18 16:49:45 -0700

    WIP. forms. form options code review

--- src/components/sections/3-controls/1-form-options.tsx
+++ src/components/sections/3-controls/1-form-options.tsx
@@ -1,38 +1,55 @@
 import { screenLoginOptionAtoms } from "@/store/store";
 import { classNames } from "@/utils";
 import { useAtom } from "jotai";
-import { Checkbox } from "./1-checkbox";
-import { LevelSwitch } from "./2-level-switch";
+import { Checkbox } from "./2-checkbox";
+import { LevelSwitch } from "./3-level-switch";
 
 export function FormOptions({ className, ...rest }: React.HTMLAttributes<HTMLDivElement>) {
     const { revealAtom, doIntervalAtom, intervalAtom, pageReloadAtom, useWebCompAtom, } = screenLoginOptionAtoms;
+
     const [reveal, setReveal] = useAtom(revealAtom);
     const [doInterval, setDoInterval] = useAtom(doIntervalAtom);
     const [interval, setInterval] = useAtom(intervalAtom);
     const [pageReload, setPageReload] = useAtom(pageReloadAtom);
     const [useWebComp, setUseWebComp] = useAtom(useWebCompAtom);
+
     return (
         <div className={classNames("px-2 py-1 text-xs bg-slate-100 border-slate-400 border rounded-sm shadow select-none", className)} {...rest}>
             <Checkbox label="Reveal passwords" checked={reveal} onChange={() => setReveal((v) => !v)} />
 
             <div className="flex items-center">
                 <Checkbox label="Reload interval" checked={doInterval} onChange={() => setDoInterval((v) => !v)} />
+
                 {doInterval
-                    ?
-                    <div className="h-[18px] pl-1 pt-0.5"><span className="font-bold">{interval}</span> sec</div>
+                    ? (
+                        <div className="h-[18px] pl-1 pt-0.5">
+                            <span className="font-bold">
+                                {interval}
+                            </span>
+                            {' '}sec
+                        </div>
+                    )
                     :
-                    <div className="pl-1 flex items-center space-x-1">
-                        <input
-                            className="w-10 px-1 border-slate-400 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-slate-500 border rounded" type="text"
-                            value={interval} onChange={((e) => setInterval(+e.target.value))}
-                            data-dbg-tm />
-                        <div className="">sec</div>
-                    </div>}
+                    (
+                        <div className="pl-1 flex items-center space-x-1">
+                            <input
+                                className="w-10 px-1 border-slate-400 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-slate-500 border rounded" type="text"
+                                value={interval} onChange={((e) => setInterval(+e.target.value))}
+                                data-dbg-tm
+                            />
+                            <div>
+                                {' '}sec
+                            </div>
+                        </div>
+                    )
+                }
             </div>
 
             <Checkbox label="Reload page vs. form" checked={pageReload} onChange={() => setPageReload((v) => !v)} />
+
             <div className="flex items-center">
                 <Checkbox label="Use WebComponents" checked={useWebComp} onChange={() => setUseWebComp((v) => !v)} />
+                    
                 <LevelSwitch className={useWebComp ? "" : "invisible"} />
             </div>
         </div>
--- src/components/sections/3-controls/4-mount.tsx
+++ src/components/sections/3-controls/4-mount.tsx
@@ -9,31 +9,45 @@ type MountProps = {
 
 /* Too many motion * /
 export function MountOptions({ showAtom, children }: MountProps) {
+
     const show = useAtomValue(showAtom);
+
     const transitions = useTransition(show ? 1 : 0, {
         from: { x: -200, opacity: 0, },
         enter: { x: 0, opacity: 1, config: { duration: 200, easing: easings.easeOutCubic }, },
         leave: { x: -100, opacity: 0, config: { duration: 200, easing: easings.easeOutQuad }, },
     });
-    return transitions((styles, item) => !!item && (
-        <a.div style={styles}>
-            {children}
-        </a.div>)
+
+    return transitions(
+        (styles, item) => {
+            return !!item && (
+                <a.div style={styles}>
+                    {children}
+                </a.div>
+            );
+        }
     );
 }
 /**/
 
 export function Mount({ showAtom, children }: MountProps) {
+
+    const doInterval = useAtomValue(screenLoginOptionAtoms.doIntervalAtom);
     const show = useAtomValue(showAtom);
-    const doIntevel = useAtomValue(screenLoginOptionAtoms.doIntervalAtom);
-    const transitions = useTransition(show && !doIntevel, {
+
+    const transitions = useTransition(show && !doInterval, {
         from: { x: -200, opacity: 0, },
         enter: { x: 0, opacity: 1, config: { duration: 150, easing: easings.easeOutCubic }, },
         leave: { x: -100, opacity: 0, config: { duration: 150, easing: easings.easeOutQuad }, /* onRest: () => console.log('done') */ },
     });
-    return transitions((styles, item) => !!item && (
-        <a.div style={styles}>
-            {children}
-        </a.div>)
+
+    return transitions(
+        (styles, item) => {
+            return !!item && (
+                <a.div style={styles}>
+                    {children}
+                </a.div>
+            );
+        }
     );
 }
