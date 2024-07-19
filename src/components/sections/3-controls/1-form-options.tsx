ac49439d97a0d39c8a0c2d066b93491b5c00b5e6
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-18 20:15:29 -0700

    WIP. forms. form options. add 3-option-interval.tsx

--- src/components/sections/3-controls/1-form-options.tsx
+++ src/components/sections/3-controls/1-form-options.tsx
@@ -3,44 +3,10 @@ import { classNames } from "@/utils";
 import { useAtom } from "jotai";
 import { Checkbox } from "./2-checkbox";
 import { LevelSwitch } from "./3-level-switch";
-
-function OptionInterval() {
-    const { revealAtom, doIntervalAtom, intervalAtom, pageReloadAtom, useWebCompAtom, } = screenLoginOptionAtoms;
-
-    const [doInterval, setDoInterval] = useAtom(doIntervalAtom);
-    const [interval, setInterval] = useAtom(intervalAtom);
-    return (
-        <div className="flex items-center">
-            <Checkbox label="Reload interval" checked={doInterval} onChange={() => setDoInterval((v) => !v)} />
-
-            {doInterval
-                ? (
-                    <div className="h-[18px] pl-1 pt-0.5">
-                        <span className="font-bold">
-                            {interval}
-                        </span>
-                        {' '}sec
-                    </div>
-                )
-                : (
-                    <div className="pl-1 flex items-center space-x-1">
-                        <input
-                            className="w-10 px-1 border-slate-400 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-slate-500 border rounded" type="text"
-                            value={interval} onChange={((e) => setInterval(+e.target.value))}
-                            data-dbg-tm
-                        />
-                        <div>
-                            {' '}sec
-                        </div>
-                    </div>
-                )
-            }
-        </div>
-    );
-}
+import { OptionInterval } from "./3-option-interval";
 
 export function FormOptions({ className, ...rest }: React.HTMLAttributes<HTMLDivElement>) {
-    const { revealAtom, doIntervalAtom, intervalAtom, pageReloadAtom, useWebCompAtom, } = screenLoginOptionAtoms;
+    const { revealAtom, pageReloadAtom, useWebCompAtom, } = screenLoginOptionAtoms;
 
     const [reveal, setReveal] = useAtom(revealAtom);
     const [pageReload, setPageReload] = useAtom(pageReloadAtom);
