d55ee34b128b581676ef2e9c180664dfa5ab3c76
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-18 17:02:04 -0700

    WIP. forms. form options. update LevelSwitch

--- src/components/sections/3-controls/3-level-switch.tsx
+++ src/components/sections/3-controls/3-level-switch.tsx
@@ -1,17 +1,44 @@
+import { ChangeEvent, HTMLAttributes } from "react";
+import { useAtom } from "jotai";
 import { screenLoginOptionAtoms } from "@/store/store";
 import { classNames } from "@/utils";
-import { useAtom } from "jotai";
-import { ChangeEvent } from "react";
 
-export function LevelSwitch({ className }: React.HTMLAttributes<HTMLUListElement>) {
-    const [nestLevel, setNestLevel] = useAtom(screenLoginOptionAtoms.nestLevelAtom);
-    const onChange = (event: ChangeEvent<HTMLInputElement>) => setNestLevel(+event.target.value);
+type OneRadioProps = {
+    value: number;
+    checked: boolean;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+function OneRadio({ value, checked, onChange }: OneRadioProps) {
     return (
-        <ul className={classNames("ml-2 flex items-center space-x-0.5", className)} title="WebComponents render level">
-            <label className="flex items-center"><input data-dbg-tm className="form-radio w-2.5 h-2.5 text-slate-400 focus:ring-1 focus:ring-offset-1 focus:ring-slate-500" type="radio" onChange={onChange} checked={nestLevel === 0} value={0} name="nestLevel" /></label>
-            <label className="flex items-center"><input data-dbg-tm className="form-radio w-2.5 h-2.5 text-slate-400 focus:ring-1 focus:ring-offset-1 focus:ring-slate-500" type="radio" onChange={onChange} checked={nestLevel === 1} value={1} name="nestLevel" /></label>
-            <label className="flex items-center"><input data-dbg-tm className="form-radio w-2.5 h-2.5 text-slate-400 focus:ring-1 focus:ring-offset-1 focus:ring-slate-500" type="radio" onChange={onChange} checked={nestLevel === 2} value={2} name="nestLevel" /></label>
-            <label className="flex items-center"><input data-dbg-tm className="form-radio w-2.5 h-2.5 text-slate-400 focus:ring-1 focus:ring-offset-1 focus:ring-slate-500" type="radio" onChange={onChange} checked={nestLevel === 3} value={3} name="nestLevel" /></label>
+        <label className="flex items-center">
+            <input
+                type="radio"
+                data-dbg-tm
+                className="form-radio w-2.5 h-2.5 text-slate-400 focus:ring-1 focus:ring-offset-1 focus:ring-slate-500"
+                value={value}
+                onChange={onChange}
+                checked={checked}
+                name={`nest-level-${value}`}
+            />
+        </label>
+    );
+}
+
+export function LevelSwitch({ className, ...rest }: HTMLAttributes<HTMLUListElement>) {
+
+    const [nestLevel, setNestLevel] = useAtom(screenLoginOptionAtoms.nestLevelAtom);
+
+    function onChange(event: ChangeEvent<HTMLInputElement>) {
+        return setNestLevel(+event.target.value);
+    }
+
+    return (
+        <ul className={classNames("ml-2 flex items-center space-x-0.5", className)} title="WebComponents render level" {...rest}>
+            <OneRadio onChange={onChange} checked={nestLevel === 0} value={0} />
+            <OneRadio onChange={onChange} checked={nestLevel === 1} value={1} />
+            <OneRadio onChange={onChange} checked={nestLevel === 2} value={2} />
+            <OneRadio onChange={onChange} checked={nestLevel === 3} value={3} />
         </ul>
     );
 }
